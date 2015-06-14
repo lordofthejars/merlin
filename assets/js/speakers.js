2f968b6810c02adfbf3ff172c241d010103b64b4
atort <adela.tort@gmail.com>
2015-06-14 19:36:51 +0200

    repeated speakers

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$.getJSON( "assets/json/speakers.json", function( data ) {
 	  $.each( data.speakers, function( key, val ) {
-	  	if (val.enabled == 1) {
+	  	if (val.enabled == 1 && val.repeated !=1) {
 			divspeaker="<div class='speaker-info col-md-3 col-sm-6 col-xs-12 teammate animated' data-animation='fadeInUp' data-delay='600'>"+
 						  "<div class='profile-photo'>";
 			if(val.url!=""){						  
